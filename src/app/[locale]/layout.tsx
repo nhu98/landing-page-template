a7380bbce29d370b0d5212ad66e65716329ec053
trungnguyenite <102409642+trungnguyenite@users.noreply.github.com>
2024-08-28 18:02:48 +0700

    test livechat

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google';
 import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
 import type { Metadata } from 'next';
+import { useEffect } from 'react';
 
 // Can be imported from a shared config
 const locales = ['vi', 'en'];
@@ -46,6 +47,22 @@ export default async function LocaleLayout({
   // side is the easiest way to get started
   const messages = await getMessages();
 
+  useEffect(() => {
+    // Re-execute the script whenever the locale changes
+    const script = document.createElement('script');
+    script.innerHTML = `
+      window.__lc = window.__lc || {};
+        window.__lc.license = 18457389;
+        (function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice));
+    `;
+    document.body.appendChild(script);
+
+    // Cleanup script on unmount
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [locale]);  // This effect runs when `locale` changes
+
   return (<html lang={locale}>
   
   <body className={`${inter.variable}  `}>
@@ -54,15 +71,7 @@ export default async function LocaleLayout({
     {children}
     <Footer />
 
-    <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.__lc = window.__lc || {};
-        window.__lc.license = 18457389;
-        (function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice));
-      `,
-    }}
-  />
+    
 
   </NextIntlClientProvider>
 
