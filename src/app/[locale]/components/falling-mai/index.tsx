39c9f740c56730cd0779f03dc4554f95a5a10aca
nhuhq <nhuhq@imt-soft.com>
2025-01-31 12:34:49 +0700

    Feat: Implement the mai falling effect cover in the landing page

--- src/app/[locale]/components/falling-mai/index.tsx
+++ src/app/[locale]/components/falling-mai/index.tsx
@@ -73,9 +73,7 @@ const FallingMai = () => {
       canvas.height = window.innerHeight;
     });
 
-    return () => {
-      window.removeEventListener('resize', () => {});
-    };
+    return;
   }, []);
 
   return (
